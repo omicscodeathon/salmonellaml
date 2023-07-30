ac00513e2924ed1d2def1f9e2ffd815a80685212
Your Name <bonfaceb21@gmail.com>
2023-07-30 18:53:16 +0000

    updated download script outputing data in one directory

--- scripts/download_3.sh
+++ scripts/download_3.sh
@@ -1,49 +1,51 @@
 #/usr/bin/env bash
 # This script uses fasterq-dump to download SRA accessions listed in the input accession file !/usr/bin/env bash
 # cpus=24 
-# country_data=../accessions/sra_accessions.txt 
-# for accession in $(cat $country_data | sed '1d') 
-# do
-#     echo "Creating directory for $accession"
+input_directory="../accessions/trial_accessions"
+output_directory="../data/raw_data"
+for file in $(ls $input_directory/*.txt) 
+do
+for accession in $(cat $file) 
+
+do
+#     echo "processing  $accession"
 #     mkdir -pv ../raw_data/$accession
 #     echo "Successfully created directory for $accession"
+    echo "Downloading $accession"
+    prefetch $accession
+#     cd ../raw_data/$accession
+    fastq-dump --outdir $output_directory --gzip $accession
+    echo "Download of the sample ended"
+    cd - # Return to the previous directory done
+done
+done
+# cpus=24
+# input_directory="../accessions/trial_accessions"
+# output_directory="../data/raw_data"
+
+# # Function to download data for a single accession
+# function download_accession_data() {
+#     local accession=$1
 #     echo "Downloading $accession"
 #     prefetch $accession
-#     cd ../raw_data/$accession
-#     fastq-dump --outdir ./ --gzip $accession
+#     fastq-dump --outdir "$output_directory" --gzip $accession
 #     echo "Download of the sample ended"
-#     cd - # Return to the previous directory done
+# }
+
+# # Loop through each file in the input directory
+# for accession_file in "$input_directory"/*.txt; do
+#     # Get the file name without the path and extension
+#     file_name=$(basename "$accession_file" .txt)
+    
+#     # Echo the file name before processing
+#     echo "Processing file: $file_name"
+
+#     # Loop through each accession in the file
+#     while IFS= read -r accession; do
+#         if [[ -n "$accession" ]]; then
+#             download_accession_data "$accession"
+#         fi
+#     done < "$accession_file"
 # done
 
-#!/usr/bin/env bash
-
-# Check if the folder argument is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <folder>"
-    exit 1
-fi
-
-cpus=24
-folder="$1"
-
-# Loop through each 'sra_accessions.txt' file in the provided folder and its subfolders
-for country_data in "$folder"/*sra_accessions.txt; do
-    echo "Processing file: $country_data"
-
-    # Loop through the accessions listed in the file
-    while read -r accession; do
-        # Skip the first line (if any) and any empty lines
-        if [[ "$accession" == "Sample"* || -z "$accession" ]]; then
-            continue
-        fi
-
-        echo "Creating directory for $accession"
-        mkdir -pv "../raw_data/$accession"
-        echo "Successfully created directory for $accession"
-
-        echo "Downloading $accession"
-        prefetch "$accession"
-        fastq-dump --outdir "../raw_data/$accession" --gzip "$accession"
-        echo "Download of the sample ended"
-    done < <(tail -n +2 "$country_data")
-done
+echo "All downloads completed!"
