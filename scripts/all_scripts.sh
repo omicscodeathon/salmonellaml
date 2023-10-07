4aec27efc5c54f8c104639e040ff879497cd7492
Your Name <bonfaceb21@gmail.com>
2023-10-07 18:05:54 +0000

    all script from download to quality control

--- scripts/all_scripts.sh
+++ scripts/all_scripts.sh
@@ -1,18 +1,19 @@
 #/usr/bin/env bash
-# 1. extract biosmaple ID
+# extract biosmaple ID
 # python extract_biosample_ids_1.py ../data/biosample_data/
 # # 2. Extract SRA_iDs 
 #  bash sra_from_biosample_2.sh
  
 # # 3. Downlod the Data using the SRA Accesions
 # bash download_3.sh ../accessions/trial_accessions/
-
+# Data download
+nextflow run ../pipeline/ --folder_path ../accessions/kenya
 # Quality control
 bash quality_control_fastp_4a.sh ../work/b4/fe53820ee22d22d1ee8ff8e2267315
 # Variant calling providing path to the trimmed data directory
 bash vcf_pipeline_5.sh ../data/trimmed_data
 
 
-# # NEXTFLOW
-# conda activate salmonella
-# nextflow run pipeline --folder_path data/biosample_data/
+# # # NEXTFLOW
+# # conda activate salmonella
+# # nextflow run pipeline --folder_path data/biosample_data/
