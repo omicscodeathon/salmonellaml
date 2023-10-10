4ae18843b0b2a1387ce113ab97686566df58b47a
Your Name <bonfaceb21@gmail.com>
2023-10-10 23:41:51 +0000

    updated files

--- scripts/all_scripts.sh
+++ scripts/all_scripts.sh
@@ -1,19 +1,14 @@
 #/usr/bin/env bash
-# extract biosmaple ID
-# python extract_biosample_ids_1.py ../data/biosample_data/
-# # 2. Extract SRA_iDs 
-#  bash sra_from_biosample_2.sh
- 
-# # 3. Downlod the Data using the SRA Accesions
-# bash download_3.sh ../accessions/trial_accessions/
 # Data download
-nextflow run ../pipeline/ --folder_path ../accessions/kenya
-# Quality control
-bash quality_control_fastp_4a.sh ../work/b4/fe53820ee22d22d1ee8ff8e2267315
+country=$1
+echo "Downloading data started at $(date)"
+nextflow run ../pipeline/ --folder_path ../accessions/$country
+echo "Downloading data started at $(date)"
+# Quality control based on downloaded data 
+echo "Perforning Quality control satsetred at $(date)"
+bash quality_control_fastp_4a.sh ../pipeline/run_salmonellaml
 # Variant calling providing path to the trimmed data directory
+echo "Perforning Quality control ended at $(date)"
+echo "Calling variant started at $(date)"
 bash vcf_pipeline_5.sh ../data/trimmed_data
-
-
-# # # NEXTFLOW
-# # conda activate salmonella
-# # nextflow run pipeline --folder_path data/biosample_data/
+echo "Calling variant ended at $(date)"
--- scripts/vcf_pipeline_5.sh
+++ scripts/vcf_pipeline_5.sh
@@ -1,140 +1,29 @@
-# #!/bin/bash
-# ##*************************************************************************##
-# ##  Step 1: Indexing the Refeence ## 
-# ##*************************************************************************##
-# input_dir="../raw_data" 
-# REF="../data/salmonella_ref.fasta"
-# mkdir ../data/variants_out/01filter
-# variants_out="../data/variants_out"
-# echo "Indexing the reference genome"
-# mkdir ../data/output_dir/
-# output_dir="../data/output_dir" 
-# #bwa index ../data/salmonella_ref.fasta
-
-# ##*************************************************************************##
-# ##  Step 2: Mapping  to the Refeence 
-# ##*************************************************************************##do
-#   echo "mapping start at $(date)" 
-# for i in $(ls $input_dir) 
-# do
-# echo $i/$i".fastq.gz"
-# bwa mem -t 4 $REF $input_dir/$i/$i".fastq.gz" | samtools view -bS -o $output_dir/$i.bam -
-
-# # bwa mem -t 4 $REF $input_dir/$i/$i".fastq.gz" | $samtools view -Sb - > $output_dir/$i.bam
-#  echo Mapping finished at $(date)
-# ##*************************************************************************##
-# ##                 Step2. sort the bam using samtools                      ##          
-# ##*************************************************************************##
-
-# echo "**samtools sort start at $(date)**"
-# samtools sort $output_dir/$i.bam -O bam -o $output_dir/$i.sorted.bam
-# # rm -f $output_dir/$i.bam
-# samtools index $output_dir/$i.sorted.bam
-# echo "**samtools sort finished at $(date)**"
-
-# ##*************************************************************************##
-# ##                 Step3. calling variants using bcftools                  ##          
-# ##*************************************************************************##
-# echo "**bcftools calling started at $(date)**"
-# bcftools mpileup -f $REF $output_dir/$i.sorted.bam | bcftools call -vm -Oz > $variants_out/$i.vcf.gz
-# echo "**bcftools calling finished at $(date)**"
-
-
-# # ##*************************************************************************##
-# # ##                        Step3. SNP_filter                                ##          
-# # ##*************************************************************************##
-
-# vcftools --gzvcf $variants_out/$i.vcf.gz --minDP 4 --max-missing 0.2 --minQ 30 --recode --recode-INFO-all --out $variants_out/01filter/$i.filter
-# ### SNPs
-# vcftools --gzvcf $variants_out/01filter/$i.filter.recode.vcf --remove-indels --recode --recode-INFO-all --out $variants_out/01filter/$i.filter.snps
-# ### Indels
-# vcftools --gzvcf $variants_out/01filter/$i.filter.recode.vcf --keep-only-indels --recode --recode-INFO-all --out $variants_out/01filter/$i.filter.indels
-
-# ### extracting GT
-# bcftools query -f '%CHROM %POS %REF %ALT [%TGT]\n' $variants_out/01filter/$i.filter.snps.recode.vcf -o $variants_out/01filter/$i.filter.snps.extract.txt
-# # bcftools query -f '%CHROM %POS %REF %ALT [%TGT]\n' $variants_out/01filter/$i.filter.indels.recode.vcf -o $variants_out/01filter/$i.filter.indels.extract.txt
-# #!/bin/bash
-
-# #!/bin/bash
-
-# ##*************************************************************************##
-# ##  Step 1: Indexing the Reference                                         ## 
-# ##*************************************************************************##
-# input_dir="../data/trimmed_data" 
-# REF="../data/salmonella_ref.fasta"
-# mkdir ../data/variants_out/01filter
-# variants_out="../data/variants_out"
-# echo "Indexing the reference genome"
-# mkdir ../data/output_dir/
-# output_dir="../data/output_dir" 
-# bwa index ../data/salmonella_ref.fasta
-
-# ##*************************************************************************##
-# ##  Step 2: Mapping  to the Refeence 
-# ##*************************************************************************##
-# echo "mapping start at $(date)" 
-
-# # Loop through the paired-end read files
-# for i in  $(ls $input_dir/*_1_trimmed.fastq.gz);
-# do
-# echo $i
-#   foward=$i
-#   reverse="${i%_1_trimmed_fastq.gz}_2_trimmed.fastq.gz"
-#   base1=$(basename "$i")
-#   out="${base1%_1_trimmed.fastq.gz}"
-
-#   # Perform read mapping using BWA-MEM and output to SAM file
-#   bwa mem -t 4 $REF "$foward" "$reverse" > "$output_dir/$out.sam"
-
-#   # Convert SAM to BAM
-#   samtools view -Sb "$output_dir/$out.sam" > "$output_dir/$out.bam"
-  
-#   # Sort the BAM file using samtools
-#   echo "**samtools sort start at $(date)**"
-#   samtools sort "$output_dir/$out.bam" -O bam -o "$output_dir/$out.sorted.bam"
-  
-#   # Remove the intermediate SAM and unsorted BAM files
-# #   rm -f "$output_dir/$out.sam" "$output_dir/$out.bam"
-  
-#   # Index the sorted BAM file
-#   samtools index "$output_dir/$out.sorted.bam"
-#   echo "**samtools sort finished at $(date)**"
-  
-#   # Call variants using bcftools
-#   echo "**bcftools calling started at $(date)**"
-#   bcftools mpileup -f $REF "$output_dir/$out.sorted.bam" | bcftools call -vm -Oz > "$variants_out/$out.vcf.gz"
-#   echo "**bcftools calling finished at $(date)**"
-
-#   # SNP filtering
-#   vcftools --gzvcf "$variants_out/$out.vcf.gz" --minDP 4 --max-missing 0.2 --minQ 30 --recode --recode-INFO-all --out "$variants_out/01filter/$out.filter"
-#   vcftools --gzvcf "$variants_out/01filter/$out.filter.recode.vcf" --remove-indels --recode --recode-INFO-all --out "$variants_out/01filter/$out.filter.snps"
-#   vcftools --gzvcf "$variants_out/01filter/$out.filter.recode.vcf" --keep-only-indels --recode --recode-INFO-all --out "$variants_out/01filter/$out.filter.indels"
-  
-#   # Extract GT (Genotype) information
-#   bcftools query -f '%CHROM %POS %REF %ALT [%TGT]\n' "$variants_out/01filter/$out.filter.snps.recode.vcf" -o "$variants_out/01filter/$out.filter.snps.extract.txt"
-#   bcftools query -f '%CHROM %POS %REF %ALT [%TGT]\n' "$variants_out/01filter/$out.filter.indels.recode.vcf" -o "$variants_out/01filter/$out.filter.indels.extract.txt"
-
-#   cut -d " " -f 2,3,4 "$variants_out/01filter/$out.filter.snps.extract.txt" > "../data/$out.snp"
- 
-# done
-
-# ## SNP file list output
-# ls ../data/*.snp > ../data/snp.lists
-
 #!/bin/bash
+# extract country from the run pipeline
+# the file with country metadata
+directory="../pipeline/run_salmonellaml"
+#  extract the country name
+country=$(find "$directory" -type f -name "*_biosamples.txt")
+# Extract the country name from the file name, removing "sra" at the beginning
+country_name=$(basename "$country" | sed 's/^sra_//;s/_.*//')
+# Make the country directory
+mkdir "../data/$country_name"
 
-##*************************************************************************##
-##  Step 1: Indexing the Reference                                         ## 
-##*************************************************************************## 
+# reference sequnce for salmonella
 REF="../data/salmonella_ref.fasta"
 mkdir -p ../data/variants_out/01filter
 variants_out="../data/variants_out"
 echo "Indexing the reference genome"
+
 mkdir -p ../data/output_dir/
 output_dir="../data/output_dir" 
-# Uncomment the following line if indexing is needed
-# bwa index "$REF"
 
+##*************************************************************************##
+##  Step 1: Indexing the Reference                                         ## 
+##*************************************************************************## 
+
+# Uncomment the following line if indexing is not needed
+bwa index "$REF"
 
 ##*************************************************************************##
 ##  Step 2: Mapping  to the Reference 
@@ -185,8 +74,16 @@ do
   bcftools query -f '%CHROM %POS %REF %ALT [%TGT]\n' "$variants_out/01filter/$out.filter.snps.recode.vcf" -o "$variants_out/01filter/$out.filter.snps.extract.txt"
   bcftools query -f '%CHROM %POS %REF %ALT [%TGT]\n' "$variants_out/01filter/$out.filter.indels.recode.vcf" -o "$variants_out/01filter/$out.filter.indels.extract.txt"
 
-  cut -d " " -f 2,3,4 "$variants_out/01filter/$out.filter.snps.extract.txt" > "../data/$out.snp"
+  cut -d " " -f 2,3,4 "$variants_out/01filter/$out.filter.snps.extract.txt" > "../data/$country/$out.snp"
 done
 
-## SNP file list output
-ls ../data/*.snp > ../data/snp.lists
+## SNP file list output to country snp file
+ls ../data/"$country"/*.snp > ../data/"$country"_snp.lists
+# remove the files in run-salmonella pipeline to process the next country
+rm -r $directory
+# remove the files in the work to process the next country
+rm -r /work
+# remove the outpu directory for stoting the intermediary files
+rm -r output_dir
+# remove trimmed data files
+rm -r ../data/trimmed_data/*.gz
